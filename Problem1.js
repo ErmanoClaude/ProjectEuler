41e48576fcdbd5379d37a72718a6a708c79b2e8c
Ermano Claude <ermano.claude@cix.csi.cuny.edu>
2020-01-09 01:11:03 -0500

    Create Problem1.js

---
+++ Problem1.js
@@ -0,0 +1,11 @@
+const multiples = () => {
+	let count = 1000;
+	let total = 0;
+	for ( var i=0; i<count; i++){
+		if( i%3 == 0 || i%5 ==0 ){
+			total = total + i;
+		}
+	}
+	return total;
+}
+let Answer = multiples();
