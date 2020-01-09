e76f6d55520f2bab96b89ceb33aed3f91c435b56
Ermano Claude <ermano.claude@cix.csi.cuny.edu>
2020-01-09 01:12:05 -0500

    Create Problem2.js

---
+++ Problem2.js
@@ -0,0 +1,14 @@
+var fibbo = () => {
+	var start = [1,2];
+	var total = 2;
+	var even = [2]
+	while(start[start.length-1] + start[start.length-2] < 4000000){
+		var current = start[start.length-1] + start[start.length-2];
+		start.push(current);
+		if( current % 2 == 0){
+			total = total + current;
+			even.push(current)
+		}
+	}
+}
+fibbo();
