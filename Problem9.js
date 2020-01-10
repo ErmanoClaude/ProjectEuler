3006d8ec66b42420343e5b88e0081ad8a7993713
Ermano Claude <ermano.claude@cix.csi.cuny.edu>
2020-01-09 20:51:58 -0500

    Update Problem9.js

--- Problem9.js
+++ Problem9.js
@@ -5,17 +5,25 @@ const pythagoreanTriplet = (n) =>
 	let a = 3;
 	let end = n;
 	let rights = [];
+	// A to 1000
 	for (a; a<end; a++)
 	{
 		for (let b = a; b<end; b++)
 		{
+			// c = √(A^2 + B^2)
 			let c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
 			if(a + b + c > 1000){
 				break;
 			}
-			if( c%1 == 0 && a + b + c == 1000 )
+			// if C is whole number means
+			// a^2 + b^2 = c^2 and check if adds to 1000
+			if( c % 1 == 0 && a + b + c == 1000 )
 			{
 				rights.push(...[a,b,c]);
+				// makes "a" higher then end to break loop
+				// and breaks this loop
+				a = 10000;
+				break;
 			}
 		}
 	}
